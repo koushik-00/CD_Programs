72554a518bfc81174fad031f05932d559751c972
unknown <ragulpretish@gmail.com>
2023-03-26 12:38:38 +0530

    LL1 added

---
+++ parsers/ll1.py
@@ -0,0 +1,74 @@
+print("E->TE'\nE'->+TE'/@\nT->FT'\nT'->*FT'/@\nF->(E)/i\n")
+global s
+global i
+s=list(input("Enter the string: "))
+i=0
+def match(a):
+    global s
+    global i
+    if(i>=len(s)):
+        return False
+    elif (s[i]==a):
+        i+=1
+        return True
+    else:
+        return False
+def F():
+    if(match("(")):
+        if(E()):
+            if(match(")")):
+                return True
+            else:
+                return False
+        else:
+            return False
+    elif match("i"):
+        return True
+    else:
+        return False
+def Tx():
+    if(match("*")):
+        if(F()):
+            if(Tx()):
+                return True
+            else:
+                return False
+        else:
+            return False
+    else:
+        return True
+
+def T():
+    if(F()):
+        if(Tx()):
+            return True
+        else:
+            return False
+    else:
+        return False
+def Ex():
+    if(match("+")):
+        if(T()):
+            if(Ex()):
+                return True
+            else:
+                return False
+        else:
+            return False
+    else:
+        return True
+def E():
+    if(T()):
+        if(Ex()):
+            return True
+        else:
+            return False
+    else:
+        return False
+if(E()):
+    if(i==len(s)):
+        print("String Accepted")
+    else:
+        print("String not accepted")
+else:
+    print("String not accepted")
