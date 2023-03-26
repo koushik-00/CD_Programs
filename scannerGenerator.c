0c951c56c0749507984bd50aefe6dd6f7135667e
unknown <ragulpretish@gmail.com>
2023-03-26 11:18:20 +0530

    Practiced Scanner Generator

--- scannerGenerator.c
+++ scannerGenerator.c
@@ -1,77 +1,60 @@
-/*STANDALONE SCANNER PROGRAM*/ 
-#include<stdio.h> 
-#include<ctype.h> 
+#include<stdio.h>
 #include<string.h>
-int main() 
-{
-    FILE *input, *output; 
-    int l=1; 
-    int t=0; 
-    int j=0; 
-    int i,flag; 
-    char ch,str[20]; 
-    input = fopen("input.txt","r"); 
-    output = fopen("output.txt","w"); 
-    char keyword[30][30] = {"int","main","if","else","do","while"}; 
-    fprintf(output,"Line no. \t Token no. \t Token \t Lexeme\n\n"); 
-    while(!feof(input))
-    {
-        i=0; 
-        flag=0; 
-        ch=fgetc(input); 
-        if( ch=='+' || ch== '-' || ch=='*' || ch=='/' )
-        {
-            fprintf(output,"%7d\t\t %7d\t\t Operator\t %7c\n",l,t,ch); 
-            t++; 
-        } 
-        else if( ch==';' || ch=='{' || ch=='}' || ch=='(' || ch==')' || ch=='?' || ch=='@' || ch=='!' || ch=='%') 
-        {
-            fprintf(output,"%7d\t\t %7d\t\t Special symbol\t %7c\n",l,t,ch); 
-            t++; 
-        } 
-        else if(isdigit(ch)) 
-        { 
-            fprintf(output,"%7d\t\t %7d\t\t Digit\t\t %7c\n",l,t,ch); 
-            t++; 
-        } 
-        else if(isalpha(ch)) 
-        { 
-            str[i]=ch; 
-            i++; 
-            ch=fgetc(input); 
-            while(isalnum(ch) && ch!=' ') 
-            { 
-                str[i]=ch; 
-                i++; 
-                ch=fgetc(input); 
-            } 
-            str[i]='\0'; 
-            for(j=0;j<=30;j++) 
-            { 
-                if(strcmp(str,keyword[j])==0) 
-                { 
-                    flag=1; 
-                    break; 
-                } 
-            } 
-            if(flag==1) 
-            { 
-                fprintf(output,"%7d\t\t %7d\t\t Keyword\t %7s\n",l,t,str); 
-                t++; 
-            } 
-            else 
-            { 
-                fprintf(output,"%7d\t\t %7d\t\t Identifier\t %7s\n",l,t,str); 
-                t++; 
-            } 
-        } 
-        else if(ch=='\n')     
-        { 
-            l++; 
-        } 
-    } 
-    fclose(input); 
-    fclose(output); 
-    return 0; 
+#include<ctype.h>
+#include<math.h>
+int main(){
+    FILE *input=fopen("input.txt","r");
+    FILE *output=fopen("output.txt","w");
+    char str[20];
+    char keyword[30][30]={"int","double","char","if","else","main"};
+    int i,flag;
+    int t=1,l=0;
+    fprintf(output,"Line no.\t\tToken no.\t\t Token\t\t Lexeme\n\n"); 
+    while(!feof(input)){
+        i=0; flag=0;
+        char ch=fgetc(input);
+        if(ch=='+'||ch=='-'||ch=='*'||ch=='/'){
+            fprintf(output,"%7d\t\t%7d\t\tOperator\t\t%7c\n\n",l,t,ch);
+            t++;
+        }
+        else if(ch=='('||ch==')'||ch=='{'||ch=='}'||ch=='%'||ch==';'){
+            fprintf(output,"%7d\t\t%7d\t\tSpecial Symbol\t\t%7c\n\n",l,t,ch);
+            t++;
+        }
+        else if(isdigit(ch)){
+            fprintf(output,"%7d\t\t%7d\t\tDigit\t\t%7c\n\n",l,t,ch);
+            t++;
+        }
+        else if(isalpha(ch)){
+            str[i]=ch;
+            i++;
+            ch=fgetc(input);
+            while(isalpha(ch) && ch!=' '){
+                str[i]=ch;
+                i++;
+                ch=fgetc(input);
+            }
+            str[i]='\0';
+            for(int j=0;j<30;j++){
+                if(strcmp(str,keyword[j])==0){
+                    flag=1;
+                    break;
+                }
+            }
+            if(flag==1){
+                fprintf(output,"%7d\t\t%7d\t\tKeyword\t\t%7s\n\n",l,t,str);
+                t++;
+            }
+            else{
+                fprintf(output,"%7d\t\t%7d\t\tIdentifier\t\t%7s\n\n",l,t,str);
+                t++;
+            }
+        }
+        else if(ch=='\n'){
+            l++;
+        }
+    }
+    fclose(input);
+    fclose(output);
+    return 0;
 }
-
